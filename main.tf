8a422c8b166f8753633afbca59f69e59372e4cda
srikanthoggari <114415677+srikanthoggari@users.noreply.github.com>
2022-11-29 22:23:45 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -54,6 +54,7 @@ resource "aws_instance" "web" {
               sed -i -e 's/80/8080/' /etc/apache2/ports.conf
               echo "Hello World" > /var/www/html/index.html
               systemctl restart apache2
+              EOF
 }
 
 resource "aws_security_group" "web-sg" {
