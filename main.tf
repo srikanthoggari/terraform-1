0f1efb5d13a584f3f4655e971e7e6cb1bb628190
srikanthoggari <114415677+srikanthoggari@users.noreply.github.com>
2022-11-29 23:30:35 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -21,7 +21,7 @@ terraform {
 }
 
 provider "aws" {
-  region = "ap-southeast-1"
+  region = "us-east-1"
 }
 
 resource "random_pet" "sg" {}
@@ -48,13 +48,15 @@ resource "aws_instance" "web" {
   vpc_security_group_ids = [aws_security_group.web-sg.id]
 
   user_data = <<-EOF
-              #!/bin/bash
-              apt-get update
-              apt-get install -y apache2
-              sed -i -e 's/80/8080/' /etc/apache2/ports.conf
-              echo "Hello World" > /var/www/html/index.html
-              systemctl restart apache2
-              EOF
+             #!/bin/bash
+              sudo apt-get update -y
+              sudo apt install docker.io -y
+              sudo apt install docker-compose -y
+              
+              echo
+              git clone https://github.com/srikanthoggari/Snipe-IT.git snipe-it
+              cd /snipe-it/
+              sudo docker-compose up
 }
 
 resource "aws_security_group" "web-sg" {
