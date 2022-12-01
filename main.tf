25c1996763bc6e3f486fb3d92382168f30e47129
srikanthoggari <114415677+srikanthoggari@users.noreply.github.com>
2022-12-01 01:20:37 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ terraform {
   required_version = ">= 1.1.0"
 
   cloud {
-    organization = "srikanthsamatha"
+    organization = ""oggarisrikanthsamath
 
     workspaces {
       name = "githubaction"
