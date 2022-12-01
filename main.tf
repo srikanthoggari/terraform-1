dad983d096456d1593d20591275a0ffeaaab889f
srikanthoggari <114415677+srikanthoggari@users.noreply.github.com>
2022-12-01 00:40:00 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -72,7 +72,7 @@ resource "aws_security_group" "web-sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-    ingress {
+  ingress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
